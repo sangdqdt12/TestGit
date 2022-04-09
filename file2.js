91acfbec2bf4c1a26555c008d22adb51f4a478ca
= <=>
2022-04-09 15:38:47 +0700

    sửa 3 file và commit một lần

--- file2.js
+++ file2.js
@@ -4,3 +4,5 @@ Sửa file 2 sau khi sửa file 1
 
 ----------------------------------
 đây là file 2
+---------------------------------------
+sửa file 3, sửa xong commit một lần
--- file3.ts
+++ file3.ts
@@ -7,3 +7,5 @@ tiep tuc sưa file 3
 sửa file 3 lần 4
 -------------------------------------
 đây là file 3
+----------------------------------
+sua file 3, sửa xong commit 1 lần
